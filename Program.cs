074b0d1745b983363c20d45b01d65ece7bd27538
khalid karam <khalid.tasks@gmail.com>
2024-10-22 16:45:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ObjectInitializer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car
+            {
+                Make = "Ford",
+                Model = "Mustang",
+                Engine = new Engine { HorsePower = 450 }
+            };
+
+        }
+    }
+
+    public class Engine
+    {
+        public int HorsePower { get; set; }
+    }
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+    }
+
+
+}
